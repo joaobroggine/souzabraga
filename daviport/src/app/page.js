74af55a313eaa391251f1f3039b0425778e0fafa
joaobroggine <joaovitorbrogginelopes@gmail.com>
2025-02-05 22:26:15 -0300

    Projeto iniciado. Header quase finalizado

---
+++ daviport/src/app/page.js
@@ -0,0 +1,10 @@
+import Header from "./components/header/header";
+
+export default function Home() {
+  return (
+    <div className="h-screen p-10
+     flex justify-center bg-gradient-to-r from-green-500 to-[#F9B572]">
+        <Header/>
+    </div>
+  );
+}
